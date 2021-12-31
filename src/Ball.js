274b3572a8702bea4de41d692e6d0fe302d1f223
manutdmohit <saudmohit@gmail.com>
2021-12-31 20:00:03 +0545

    ball stephen

--- src/Ball.js
+++ src/Ball.js
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 const Ball = () => {
-  const [position] = useState(new Animated.ValueXY(0, 0));
+  const position = useState(new Animated.ValueXY(0, 0))[0];
 
-  React.useEffect(() => {
+  const moveBall = () => {
     Animated.spring(position, {
-      toValue: { x: 200, y: 500 },
+      toValue: { x: 250, y: 500 },
+
       useNativeDriver: false,
     }).start();
-  }, []);
+  };
 
   return (
-    <Animated.View style={position.getLayout()}>
-      <View style={styles.ball} />
-    </Animated.View>
+    <>
+      <Animated.View style={position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
+      <TouchableOpacity onPress={moveBall}>
+        <Text>Press Me</Text>
+      </TouchableOpacity>
+    </>
   );
 };
 
