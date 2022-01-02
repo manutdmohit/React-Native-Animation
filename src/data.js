4e499edcbc0404b761749be92179324f5f298f7b
manutdmohit <saudmohit@gmail.com>
2022-01-02 16:50:02 +0545

    data

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    id: 1,
+    text: 'Card #1',
+    uri: 'https://i.pinimg.com/originals/37/f9/b8/37f9b89a8dce55eeed9c2e34801e8afd.jpg',
+  },
+  {
+    id: 2,
+    text: 'Card #2',
+    uri: 'http://www.gossipall.com/uploads/1292157007/gallery_1_2_39490.jpg',
+  },
+  {
+    id: 3,
+    text: 'Card #3',
+    uri: 'https://picsum.photos/seed/picsum/200/300',
+  },
+  {
+    id: 4,
+    text: 'Card #4',
+    uri: 'https://picsum.photos/seed/picsum/200/300',
+  },
+  {
+    id: 5,
+    text: 'Card #5',
+    uri: 'https://picsum.photos/seed/picsum/200/300',
+  },
+  {
+    id: 6,
+    text: 'Card #6',
+    uri: 'https://picsum.photos/seed/picsum/200/300',
+  },
+  {
+    id: 7,
+    text: 'Card #7',
+    uri: 'https://picsum.photos/seed/picsum/200/300',
+  },
+  {
+    id: 8,
+    text: 'Card #8',
+    uri: 'https://picsum.photos/seed/picsum/200/300',
+  },
+];
