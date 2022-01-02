44b64c92a1229ca2c887e6b395f6ff54fd0b4f11
manutdmohit <saudmohit@gmail.com>
2022-01-02 16:52:00 +0545

    understanding panresponder

---
+++ src/Deck.js
@@ -0,0 +1,51 @@
+import React, { useRef } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  PanResponder,
+  Animated,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Card, Button, Image } from 'react-native-elements';
+
+import { data } from './data';
+
+const Deck = () => {
+  const pan = useRef(new Animated.ValueXY()).current();
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: () => {},
+      onPanResponderRelease: () => {},
+    })
+  ).current;
+
+  return (
+    <SafeAreaView>
+      {
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => {
+            return (
+              <Card>
+                <Card.Image source={{ uri: item.uri }}></Card.Image>
+                <Card.Title>{item.text}</Card.Title>
+                <Text style={{ marginBottom: 10 }}>This is customizable</Text>
+                <Button icon={{ name: 'code' }} title="View Now" />
+              </Card>
+            );
+          }}
+        />
+      }
+      {...panResponder.panHandlers}
+    </SafeAreaView>
+  );
+};
+
+export default Deck;
+
+const styles = StyleSheet.create({});
