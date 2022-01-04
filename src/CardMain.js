5f7413ec5b7e81ae1a233148a979452f978729bd
manutdmohit <saudmohit@gmail.com>
2022-01-04 10:24:41 +0545

    styling card

--- src/CardMain.js
+++ src/CardMain.js
@@ -4,9 +4,20 @@ import { Text, Card, Button } from 'react-native-elements';
 const CardMain = ({ url, text }) => {
   return (
     <Card>
-      <Card.Image source={{ uri: url }}></Card.Image>
+      <Card.Image
+        source={{ uri: url }}
+        style={{
+          height: 150,
+          width: 150,
+          borderRadius: 75,
+          marginHorizontal: 65
+          ,
+        }}
+      ></Card.Image>
       <Card.Title>{text}</Card.Title>
-      <Text style={{ marginBottom: 10 }}>This is customizable</Text>
+      <Text style={{ marginBottom: 10, textAlign: 'center' }}>
+        This is customizable
+      </Text>
       <Button icon={{ name: 'code' }} title="View Now" />
     </Card>
   );
